e07c8b4d041537ae55416aec0351c6c9279fc69e
alan <alan5899f@gmail.com>
2023-10-21 00:03:05 +0800

    Change Token

--- RepositorySearchApp/Library/Config.swift
+++ RepositorySearchApp/Library/Config.swift
@@ -14,9 +14,4 @@ let Height: CGFloat = UIScreen.main.bounds.height
 
 let Base_URL = "https://api.github.com/"
 
-let Token1 = "ghp_9mCt5U7"
-let Token2 = "cPcbUNdWhQl"
-let Token3 = "vTVz9BlOMd"
-let Token4 = "xG35vHJg"
-
-let Api_Token = Token1 + Token2 + Token3 + Token4
+let Api_Token = "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"
